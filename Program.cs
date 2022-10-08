6262850d0683086d02dcd8f7bf7175e8c0924d91
John Georgesen <jwgeorgesen@gmail.com>
2022-10-08 13:12:02 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorldApp 
+{
+    class Geeks 
+    {
+        static void Main(string[] args) 
+        {
+            string[] busCoors = System.IO.File.ReadAllLines(@"busstops.txt");
+            
+            var tupleList = new List<Tuple<double, double>>();
+
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in busCoors)
+            {
+                string longitude, latitude;
+
+                longitude = line.Substring(0, 9);
+                latitude = line.Substring(10,9);
+
+                Tuple<double, double> addTup = Tuple.Create(Convert.ToDouble(longitude), Convert.ToDouble(latitude));
+                tupleList.Add(addTup);
+            }
+        }
+    }
+}
