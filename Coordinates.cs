fbbd64fd6b67ce8d782145b75074c0a19fc40465
John Georgesen <jwgeorgesen@gmail.com>
2022-10-08 14:32:10 -0400

    Another Fix

--- Coordinates.cs
+++ Coordinates.cs
@@ -2,17 +2,17 @@ namespace BusStopProgram
 {
     public class Coordinates 
     {
-        List<Tuple<double,double>> coorsList;
+        List<Tuple<decimal,decimal>> coorsList;
 
-        public List<Tuple<double,double>> getList()
+        public List<Tuple<decimal,decimal>> getList()
         {
             return getFileTuples();
         }
-        private List<Tuple<double,double>> getFileTuples()
+        private List<Tuple<decimal,decimal>> getFileTuples()
         {
             string[] busCoors = System.IO.File.ReadAllLines(@"busstops.txt");
             
-            var tupleList = new List<Tuple<double, double>>();
+            var tupleList = new List<Tuple<decimal, decimal>>();
 
             System.Console.WriteLine("Contents of WriteLines2.txt = ");
             foreach (string line in busCoors)
@@ -22,10 +22,30 @@ namespace BusStopProgram
                 longitude = line.Substring(0, 9);
                 latitude = line.Substring(10,9);
 
-                Tuple<double, double> addTup = Tuple.Create(Convert.ToDouble(longitude), Convert.ToDouble(latitude));
+                Tuple<decimal, decimal> addTup = Tuple.Create(Convert.ToDecimal(longitude), Convert.ToDecimal(latitude));
                 tupleList.Add(addTup);
             }
+
+            coorsList = tupleList;
             return tupleList;
+        }
+        public void findClosestStop(Tuple<decimal, decimal> passedTup)
+        {
+            decimal longDistance = Math.Abs(passedTup.Item1 - coorsList[0].Item1);
+            int indexOfShortest = 0;
+            //decimal latDistance = passedTup.Item2 - closestStop.Item2;
+
+            for(int i = 0; i < coorsList.Count; i++)
+            {
+                decimal tempLongDistance = Math.Abs(passedTup.Item1 - coorsList[i].Item1);
+
+                if(tempLongDistance < longDistance)
+                {
+                    longDistance = tempLongDistance;
+                    indexOfShortest = i;
+                }
+            }
+            Console.WriteLine(indexOfShortest + ": " + longDistance);
         }
         
     }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,31 @@ namespace BusStopProgram
         static void Main(string[] args) 
         {
             Coordinates coorsClass = new Coordinates();
-            List<Tuple<double,double>> coorsList = coorsClass.getList();
+            List<Tuple<decimal,decimal>> coorsList = coorsClass.getList();
 
             for(int i = 0; i < coorsList.Count; i++)
             {
                 Console.WriteLine(coorsList[i]);
             }
+
+            Console.Write("What are your current GPS Coordinates?: ");
+            String userCoors = Console.ReadLine();
+
+            userCoors = String.Concat(userCoors.Where(c => !Char.IsWhiteSpace(c)));
+
+            string longitude, latitude;
+
+            int commaSpace = userCoors.IndexOf(',');
+            int l = userCoors.Length;
+
+            longitude = userCoors.Substring(0, commaSpace);
+            latitude = userCoors.Substring(commaSpace+1,l-commaSpace-1);
+
+            Tuple<decimal, decimal> userCoorTup = Tuple.Create(Convert.ToDecimal(longitude), Convert.ToDecimal(latitude));
+
+            coorsClass.findClosestStop(userCoorTup);
+
+            Console.WriteLine(userCoorTup);
         }
         
         
